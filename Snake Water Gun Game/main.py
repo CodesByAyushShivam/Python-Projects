3ff08880802bad0914776073dadc17bc3c219923
Ayush Shivam <ayushshivam7245@gmail.com>
2025-07-12 15:13:33 +0530

    Create main.py

---
+++ Snake Water Gun Game/main.py
@@ -0,0 +1,34 @@
+import random
+
+won = "You won!"
+lost = "You lost!"
+computer = random.choice([1, 2, 3])
+
+youDict = {"s": 1, "w": 2, "g": 3}
+reverseDict = {1: "Snake", 2: "Water", 3: "Gun"}
+
+print("Send s for Snake\nw for water\ng for gun")
+youStr = input("Enter your choice: ")
+
+if (len(youStr) == 1 and (youStr.lower()=="s" or youStr.lower()=="w" or youStr.lower()=="g")):
+    you = youDict[youStr.lower()]
+    print(f"You chose {reverseDict[you]} and Computer chose {reverseDict[computer]}")
+    if (computer==you):
+        print("Its a draw!")
+    elif (computer==1 and you==2):
+        print(lost)
+    elif (computer==1 and you==3):
+        print(won)
+    elif (computer==2 and you==1):
+        print(won)
+    elif (computer==2 and you==3):
+        print(lost)
+    elif (computer==3 and you==2):
+        print(won)
+    elif (computer==3 and you==1):
+        print(lost)
+    else:
+        print("Something went wrong")
+
+else:
+    print("Your input is invalid! Run the program again and play correctly as instructed.")
