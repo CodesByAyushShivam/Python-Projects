4174e561ffd7f991f5be1c701dc1e520a9f1abca
Ayush Shivam <ayushshivam7245@gmail.com>
2025-07-24 23:41:41 -0700

    Create main.py

---
+++ Password Strength Cheaker/main.py
@@ -0,0 +1,26 @@
+import string
+
+password = input("Enter Your Password: ")
+
+strength = 0
+
+if any(a.islower() for a in password) and any(a.isupper() for a in password):
+    strength += 1
+
+if any(a.isdigit() for a in password):
+    strength += 1
+
+if len(password) >= 8:
+    strength += 1
+
+if any(char in string.punctuation for char in password):
+    strength += 1
+
+if strength <= 1:
+    print("Your password is weak")
+elif strength == 2 or strength == 3:
+    print("Your password is moderate")
+else:
+    print("Your password is strong")
+
+print(f"Your Password Strength rating is {strength}/4")
